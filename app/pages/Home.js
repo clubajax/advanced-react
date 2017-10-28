6a361ec4fa4d8dc2b32f02cadd72cc4c3c4865d5
Mike Wilcox <anm9tr@gmail.com>
2017-10-28 14:16:20 -0500

    add table

--- app/pages/Home.js
+++ app/pages/Home.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import '@clubajax/data-table';
+import getData from '../util/data-20';
+
+
+const columns = 'firstName,lastName,company,city,state,zipcode'.split(',');
 
 export default function Home () {
 	return (
 		<main>
-			Home Page
+			<h2>Home Page</h2>
+			<data-table
+				data={getData(columns)}
+				scrollable={true}
+				sortable={true}
+				stretch-column="company"
+			/>
 		</main>
 	)
 }
