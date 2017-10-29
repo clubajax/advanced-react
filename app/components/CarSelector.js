6f417a0c5bef1bb292fbbc967b9e07a8ab95455d
Mike Wilcox <anm9tr@gmail.com>
2017-10-29 08:28:01 -0500

    wire up drop-downs

--- app/components/CarSelector.js
+++ app/components/CarSelector.js
@@ -2,19 +2,40 @@ import React, { Component } from 'react';
 import WebComponent from '@clubajax/react-web-component';
 import '@clubajax/popup-list/src/drop-down';
 import { makes, models } from '../util/car-data';
+import bind from '../util/bind';
 
 export default class CarSelector extends Component {
 
 	constructor () {
 		super();
+		this.state = {
+			models: []
+		};
+		bind(this, 'onChange');
+	}
+
+	onChange (e) {
+		console.log('cng', e.target.value);
+		this.setState({ models: models[e.target.value] });
 	}
 
 	render () {
 		return (
-			<WebComponent
-				component="drop-down"
-				data={makes}
-			/>
+			<div className="car-selector">
+				<WebComponent
+					component="drop-down"
+					label="Make"
+					placeholder="Choose Make"
+					data={makes}
+					onChange={this.onChange}
+				/>
+				<WebComponent
+					component="drop-down"
+					label="Model"
+					placeholder="Choose Model"
+					data={this.state.models}
+				/>
+			</div>
 		);
 	}
 }
