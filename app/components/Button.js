049847ddbf5f86caf08d6618460dc474424e6c0c
Mike Wilcox <anm9tr@gmail.com>
2017-11-05 16:11:42 -0600

    Button - only pass button props

--- app/components/Button.js
+++ app/components/Button.js
@@ -1,6 +1,12 @@
 import React from 'react';
 
-export default function Button ({ busyText, busy, title, children, ...all }) {
+export default function Button (props) {
+	const { busyText, busy, title, type, children } = props;
+	console.log('busy:', busy, typeof busy);
+	const all = {
+		disabled: false,
+		type: type || 'submit'
+	};
 	let content = title || children;
 	if (busy) {
 		content = (<span><span className="spinner" /><span>{busyText || 'Loading...'}</span></span>) ;
